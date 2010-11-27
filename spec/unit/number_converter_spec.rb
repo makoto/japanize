468ab2108760d4bd9b16f8283ba7b12af208ca8b
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-11-27 12:23:16 +0000

    Handle different numbers

--- spec/unit/number_converter_spec.rb
+++ spec/unit/number_converter_spec.rb
@@ -2,10 +2,24 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 class NumberConverter
+
+  NUMBERS = {
+    "１" =>"1",
+    "２" =>"2",
+    "３" =>"3",
+    "４" =>"4",
+    "５" =>"5",
+    "６" =>"6",
+    "７" =>"7",
+    "８" =>"8",
+    "９" =>"9",
+    "０" =>"0"
+  }
+
   def self.convert(string)
     converted = ''
     string.each_char do |s|
-      converted << "1" if s == "１"
+      converted << NUMBERS[s] 
     end
     converted.to_i
   end
@@ -20,4 +34,9 @@ describe "Japanization" do
   it "must convert multiple value" do
     NumberConverter.convert("１１").must_equal 11
   end
+  
+  it "must convert other value" do
+    NumberConverter.convert("２").must_equal 2
+  end
+  
 end
