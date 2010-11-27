e8a03ef379ff2f593ec7fb5a3b02c4c08dc298aa
Makoto Inoue <makoto@new-bamboo.co.uk>
2010-11-27 14:51:16 +0000

    Changed operaands

--- spec/unit/evaluator_spec.rb
+++ spec/unit/evaluator_spec.rb
@@ -21,19 +21,13 @@ class Evaluator
   end
 
   def dispatch insn
-    case insn
-    when :add
-      push shift + shift
-    when :sub
-      push shift - shift
-    when :mul
-      push shift * shift
-    when :div
-      push shift / shift
+    if [:+, :-, :*, :/].include?(insn)
+      push (shift).send(insn, shift)
     else
       push insn
     end
     p @stack
+    p @pc
     @pc += 1
   end
 
@@ -53,27 +47,27 @@ end
 describe Evaluator do
 
   it "must calculate single operation" do
-    Evaluator.new([1, 2, :add]).evaluate.must_equal 3
+    Evaluator.new([1, 2, :+]).evaluate.must_equal 3
   end
 
   it "must calculate multiple operations" do
-    Evaluator.new([1, 2, :add, 3 , :mul]).evaluate.must_equal 9
+    Evaluator.new([1, 2, :+, 3 , :*]).evaluate.must_equal 9
   end
 
   it "must calculate div operations" do
-    Evaluator.new([6, 2, :div]).evaluate.must_equal 3
+    Evaluator.new([6, 2, :/]).evaluate.must_equal 3
   end
 
   it "must calculate sub operations" do
-    Evaluator.new([6, 2, :sub]).evaluate.must_equal 4
+    Evaluator.new([6, 2, :-]).evaluate.must_equal 4
   end
 
   it "must calculate float " do
-    Evaluator.new([1.5, 2, :add]).evaluate.must_equal 3.5
+    Evaluator.new([1.5, 2, :+]).evaluate.must_equal 3.5
   end
 
   it "must calculate all operands" do
-    Evaluator.new([1, 2, :add, 3 , :mul , 1, :sub, 2, :div]).
+    Evaluator.new([1, 2, :+, 3 , :* , 1, :-, 2, :/]).
       evaluate.must_equal ((((1+2) * 3) - 1 ) / 2)
   end
 end
